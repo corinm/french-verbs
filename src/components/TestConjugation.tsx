d448ff82ed2e0d7f477de2ac80146dfe3085ae6f
Corin Mulliss <corin.mulliss@gmail.com>
2020-06-10 17:47:51 +0100

    Split out useQuestion hook

--- src/components/TestConjugation.tsx
+++ src/components/TestConjugation.tsx
@@ -1,78 +1,8 @@
-import React, { useState } from "react";
-import { Input } from "semantic-ui-react";
+import React from "react";
+import { Input, Header } from "semantic-ui-react";
 
+import useQuestion from "./useQuestion";
 import { Verb } from "../types";
-import {
-  randomVerb,
-  randomConjugation,
-  randomLanguage,
-  getPronoun,
-} from "./helpers";
-
-const useQuestion = (verbs: Verb[]) => {
-  const [question, setQuestion] = useState<string>();
-  const [answer, setAnswer] = useState<string>();
-  const [guess, setGuess] = useState("");
-  const [hasSubmittedAnswer, setHasSubmittedAnswer] = useState(false);
-  const [isCorrect, setIsCorrect] = useState(false);
-
-  const newQuestion = () => {
-    const verbIndex = randomVerb(verbs);
-    const verb = verbs[verbIndex];
-    const conjugation = randomConjugation();
-    const verbByConjugation = verb[conjugation];
-    const [questionLanguage, answerLanguage] = randomLanguage();
-
-    const questionPronoun = getPronoun(
-      conjugation,
-      questionLanguage,
-      verbByConjugation.concatenate
-    );
-    const answerPronoun = getPronoun(
-      conjugation,
-      answerLanguage,
-      verbByConjugation.concatenate
-    );
-
-    const questionWord = verbs[verbIndex][conjugation][questionLanguage];
-    const answerWord = verbs[verbIndex][conjugation][answerLanguage];
-    const question = `${questionPronoun}${questionWord}`;
-    const answer = `${answerPronoun}${answerWord}`;
-    setQuestion(question);
-    setAnswer(answer);
-    setGuess("");
-    setHasSubmittedAnswer(false);
-  };
-
-  if (!question) {
-    newQuestion();
-  }
-
-  const onSubmit = () => {
-    if (hasSubmittedAnswer) {
-      newQuestion();
-    } else {
-      if (guess !== "") {
-        setHasSubmittedAnswer(true);
-        if (guess === answer) {
-          setIsCorrect(true);
-        } else {
-          setIsCorrect(false);
-        }
-      }
-    }
-  };
-
-  return {
-    question,
-    answer,
-    guess,
-    setGuess,
-    hasSubmittedAnswer,
-    isCorrect,
-    onSubmit,
-  };
-};
 
 const TestConjugation: React.FC<{ verbs: Verb[] }> = ({ verbs }) => {
   const {
@@ -96,14 +26,30 @@ const TestConjugation: React.FC<{ verbs: Verb[] }> = ({ verbs }) => {
 
   return (
     <div>
-      <div>
-        <div>Q: {question}</div>
-        <div>A: {answer}</div>
-        <div>Correct? {hasSubmittedAnswer ? isCorrect.toString() : null}</div>
-      </div>
+      <Header as="h1">{question}</Header>
+      <Feedback
+        hasSubmittedAnswer={hasSubmittedAnswer}
+        isCorrect={isCorrect}
+        answer={answer}
+      />
       <Input value={guess} onChange={onInputChange} onKeyPress={onKeyPress} />
     </div>
   );
+};
+
+const Feedback: React.FC<{
+  hasSubmittedAnswer: Boolean;
+  isCorrect: Boolean;
+  answer: string | undefined;
+}> = ({ hasSubmittedAnswer, isCorrect, answer }) => {
+  if (hasSubmittedAnswer) {
+    if (isCorrect) {
+      return <span>{"✅"}</span>;
+    } else {
+      return <span>{`Answer should have been: ${answer}`}</span>;
+    }
+  }
+  return null;
 };
 
 export default TestConjugation;
---
+++ src/components/useQuestion.ts
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { Verb } from "../types";
+import {
+  randomVerb,
+  randomConjugation,
+  randomLanguage,
+  getPronoun,
+} from "./helpers";
+
+const useQuestion = (verbs: Verb[]) => {
+  const [question, setQuestion] = useState<string>();
+  const [answer, setAnswer] = useState<string>();
+  const [guess, setGuess] = useState("");
+  const [hasSubmittedAnswer, setHasSubmittedAnswer] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(false);
+
+  const newQuestion = () => {
+    const verbIndex = randomVerb(verbs);
+    const verb = verbs[verbIndex];
+    const conjugation = randomConjugation();
+    const verbByConjugation = verb[conjugation];
+    const [questionLanguage, answerLanguage] = randomLanguage();
+
+    const questionPronoun = getPronoun(
+      conjugation,
+      questionLanguage,
+      verbByConjugation.concatenate
+    );
+    const answerPronoun = getPronoun(
+      conjugation,
+      answerLanguage,
+      verbByConjugation.concatenate
+    );
+
+    const questionWord = verbs[verbIndex][conjugation][questionLanguage];
+    const answerWord = verbs[verbIndex][conjugation][answerLanguage];
+    const question = `${questionPronoun}${questionWord}`;
+    const answer = `${answerPronoun}${answerWord}`;
+    setQuestion(question);
+    setAnswer(answer);
+    setGuess("");
+    setHasSubmittedAnswer(false);
+  };
+
+  if (!question) {
+    newQuestion();
+  }
+
+  const onSubmit = () => {
+    if (hasSubmittedAnswer) {
+      newQuestion();
+    } else {
+      if (guess !== "") {
+        setHasSubmittedAnswer(true);
+        if (guess === answer) {
+          setIsCorrect(true);
+        } else {
+          setIsCorrect(false);
+        }
+      }
+    }
+  };
+
+  return {
+    question,
+    answer,
+    guess,
+    setGuess,
+    hasSubmittedAnswer,
+    isCorrect,
+    onSubmit,
+  };
+};
+
+export default useQuestion;
