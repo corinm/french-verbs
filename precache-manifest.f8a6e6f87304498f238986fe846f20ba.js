3ba795ca39aa5e2a545b5d5d5e4446060ee458a2
Corin Mulliss <corin.mulliss@gmail.com>
2020-06-12 20:48:32 +0100

    Updates

---
+++ precache-manifest.f8a6e6f87304498f238986fe846f20ba.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a307fdacd1b87f14e4807c88de40ebaa",
+    "url": "/french-verbs/index.html"
+  },
+  {
+    "revision": "ec57676be0af58ada4b8",
+    "url": "/french-verbs/static/css/2.83553d5a.chunk.css"
+  },
+  {
+    "revision": "ec57676be0af58ada4b8",
+    "url": "/french-verbs/static/js/2.aa33d398.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/french-verbs/static/js/2.aa33d398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41b406a630bb14a995d",
+    "url": "/french-verbs/static/js/main.10e4912a.chunk.js"
+  },
+  {
+    "revision": "7fda32059a0836a9da01",
+    "url": "/french-verbs/static/js/runtime-main.e008f16d.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/french-verbs/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/french-verbs/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/french-verbs/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/french-verbs/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/french-verbs/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/french-verbs/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/french-verbs/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/french-verbs/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/french-verbs/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/french-verbs/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/french-verbs/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/french-verbs/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/french-verbs/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/french-verbs/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/french-verbs/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/french-verbs/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
